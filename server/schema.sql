577f5658b33cbdd5bd57cb78d5498421d8e7fbd9
kimberlygha <kimberlygha@gmail.com>
2015-10-30 16:28:46 -0700

    Completed SQL Schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,16 +3,22 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
   /* 
     REMEBER: A message can only have ONE user
              So add the user to the message
              HOT THE OTHER WAY AROUND.
   */
-
-  /*
-    TODO: Create fields (username_foreignKey, message, roomname)
-  */
+  id int NOT NULL AUTO_INCREMENT,
+  msg varchar(140), 
+  room_id int,
+  user_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id)
+    REFERENCES rooms(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (user_id)
+    REFERENCES users(id) 
+    ON DELETE CASCADE,
 );
 
 
@@ -22,6 +28,18 @@ CREATE TABLE messages (
   TODO: Create Table Users
   TODO: Create Table Rooms // extra
 */
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT, 
+  userName varchar(20), 
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT, 
+  roomName varchar(20), 
+  PRIMARY KEY (id)
+);
+
 
 
 /*  Execute this file from the command line by typing:
